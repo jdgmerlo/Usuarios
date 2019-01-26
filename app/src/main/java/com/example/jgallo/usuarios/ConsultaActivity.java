7b973f3a5007bad2f8719a052520b77c9c967593
David <jdgmerlo@gmail.com>
2019-01-26 00:59:49 -0300

    Commit

--- app/src/main/java/com/example/jgallo/usuarios/ConsultaActivity.java
+++ app/src/main/java/com/example/jgallo/usuarios/ConsultaActivity.java
@@ -1,6 +1,5 @@
 package com.example.jgallo.usuarios;
 
-import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
@@ -39,59 +38,10 @@ public class ConsultaActivity extends AppCompatActivity {
         switch (view.getId())
         {
             case R.id.btnBuscar:
-                //consultar();
-                consultarSQL();
-                break;
-            case R.id.btnActualizar:
-                actualizar();
-                break;
-            case R.id.btnEliminar:
-                eliminar();
+                consultar();
                 break;
         }
 
-    }
-
-    private void actualizar() {
-
-        SQLiteDatabase db = conn.getWritableDatabase();
-        String[] parametros = {et1.getText().toString()};
-        ContentValues values = new ContentValues();
-        values.put(Utilidades.C_NOMBRE,et2.getText().toString());
-        values.put(Utilidades.C_TELEFONO,et3.getText().toString());
-
-        db.update(Utilidades.T_USUARIOS,values,Utilidades.C_ID+"=?",parametros);
-        Toast.makeText(getApplicationContext(),"Actualizado.",Toast.LENGTH_LONG).show();
-        db.close();
-    }
-
-    private void eliminar() {
-        SQLiteDatabase db = conn.getWritableDatabase();
-        String[] parametros = {et1.getText().toString()};
-
-        db.delete(Utilidades.T_USUARIOS,Utilidades.C_ID+"=?",parametros);
-        Toast.makeText(getApplicationContext(),"Eliminado.",Toast.LENGTH_LONG).show();
-        db.close();
-
-    }
-
-    private void consultarSQL() {
-
-        SQLiteDatabase db = conn.getReadableDatabase();
-        String[] parametros = {et1.getText().toString()};
-
-
-        try {
-            Cursor cursor = db.rawQuery("SELECT "+Utilidades.C_NOMBRE+","+Utilidades.C_TELEFONO+" FROM " + Utilidades.T_USUARIOS + " WHERE "+ Utilidades.C_ID+ " =? ",parametros);
-
-            cursor.moveToFirst();
-            et2.setText(cursor.getString(0));
-            et3.setText(cursor.getString(1));
-
-        }catch (Exception ex){
-            Toast.makeText(getApplicationContext(),"No existe",Toast.LENGTH_LONG).show();
-            limpiar();
-        }
 
 
     }
