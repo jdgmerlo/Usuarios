5bdf498f5d28a7f75f7ec82218b290fe94d0e73a
jgallo <jdgmerlo@gmail.com>
2019-01-28 16:50:48 -0300

    Commit

--- app/src/main/java/com/example/jgallo/usuarios/entidades/Usuario.java
+++ app/src/main/java/com/example/jgallo/usuarios/entidades/Usuario.java
@@ -1,6 +1,8 @@
 package com.example.jgallo.usuarios.entidades;
 
-public class Usuario {
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
 
     private Integer id;
     private String nombre;
