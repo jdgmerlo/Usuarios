53c1c864d59e55f340edda2e359c6ef62ce06f76
jgallo <jdgmerlo@gmail.com>
2019-01-29 10:48:23 -0300

    Commit

--- app/src/main/java/com/example/jgallo/usuarios/ConexionSQLiteHelper.java
+++ app/src/main/java/com/example/jgallo/usuarios/ConexionSQLiteHelper.java
@@ -18,12 +18,14 @@ public class ConexionSQLiteHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
 
         db.execSQL(Utilidades.CREAR_TABLA_USUARIO);
+        db.execSQL(Utilidades.CREAR_TABLA_MASCOTA);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS usuarios");
+        db.execSQL("DROP TABLE IF EXISTS mascota");
         onCreate(db);
     }
 }
--- app/src/main/java/com/example/jgallo/usuarios/utilidades/Utilidades.java
+++ app/src/main/java/com/example/jgallo/usuarios/utilidades/Utilidades.java
@@ -2,10 +2,25 @@ package com.example.jgallo.usuarios.utilidades;
 
 public class Utilidades {
 
+    //USUARIOS
     public static final String T_USUARIOS = "usuarios";
     public static final String C_ID = "id";
     public static final String C_NOMBRE = "nombre";
     public static final String C_TELEFONO = "telefono";
 
     public static final String CREAR_TABLA_USUARIO = "CREATE TABLE "+ T_USUARIOS +" ("+C_ID+" INTEGER, "+C_NOMBRE+" TEXT, "+C_TELEFONO+" TEXT)";
+
+
+    //MASCOTAS
+    public static final String T_MASCOTA = "mascota";
+    public static final String C_ID_MASCOTA = "id_mascota";
+    public static final String C_NOMBRE_MASCOTA = "nombre_mascota";
+    public static final String C_RAZA_MASCOTA = "raza_mascota";
+    public static final String C_ID_DUENIO = "id_duenio";
+
+    public static final String CREAR_TABLA_MASCOTA = "CREATE TABLE " +
+            ""+T_MASCOTA+" ("+C_ID_MASCOTA+" INTEGER PRIMARY KEY AUTOINCREMENT, "
+            +C_NOMBRE_MASCOTA+" TEXT, "+C_RAZA_MASCOTA+" TEXT, "+C_ID_DUENIO+" INTEGER)";
+
+
 }
